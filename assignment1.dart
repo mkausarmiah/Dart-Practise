391b486e1585b2a713bc9e3a9c93b868e9582cce
Kausar <148365212+mkausarmiah@users.noreply.github.com>
2025-12-30 23:45:32 +0600

    Add files via upload

---
+++ assignment1.dart
@@ -0,0 +1,16 @@
+void main() {
+
+  Set<int> setA = {1, 2, 3, 4, 5};
+
+  Set<int> setB = {4, 5, 6, 7, 8};
+
+  print("***** Set A *****");
+  for (int item in setA) {
+    print(item);
+  }
+
+  print("***** Set B *****S");
+  for (int item in setB) {
+    print(item);
+  }
+}
